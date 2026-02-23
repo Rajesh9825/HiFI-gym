4b3ba443e0f8352bf58d1962713e16c424932890
RajeshGajengi <gajengi95@gmail.com>
2026-02-23 20:56:04 +0530

    github action deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +12,8 @@ export default defineConfig({
       },
     }),
   ],
+})
+
+export default defineConfig({
+  base: '/HiFI-gym/',
 })
