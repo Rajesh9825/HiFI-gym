c82c3cbd51dab803ceed64b48d13fd2a477bab60
RajeshGajengi <gajengi95@gmail.com>
2026-02-23 21:01:46 +0530

    base changed

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
-
-export default defineConfig({
-  base: '/HiFI-gym/',
+  plugins: [react()],
+  base: '/HiFi-Gym/',   // 👈 IMPORTANT (your repo name)
 })
